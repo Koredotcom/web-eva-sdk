97e998d303036ac4efebb7d6b61e54ba680775c8
Shariqmo-kore <shariq.mohd@kore.com>
2024-09-23 18:53:07 +0530

    fixes

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,3 +1,5 @@
 export { default as ChatInterface } from './ChatInterface.js';
 export { default as NewChat } from './NewChat.js';
 export { default as InitiateChatConversationAction } from './InitiateChatConversationAction.js';
+export { default as InvokeGptAgentTemplate } from './invokeGptAgentTemplate.js';
+export { default as cancelMessageReqAction } from './cancelAdvanceSearch.js';
--- src/test-comp/WelcomeTemplate.jsx
+++ src/test-comp/WelcomeTemplate.jsx
@@ -9,14 +9,10 @@ const AgentWelcomeTemplate = (props) => {
         <>
 
             <div className='threadName maxLength'>{item?.answer}</div>
-
             <div className='threadName maxLength'>
                 <div className='Answerschip msutteranceChip'>
-                    {/* {item?.templateInfo?.suggestions?.[0]?.utterances?.length} */}
                     <div className='ansdocwrap'>
-                        {/* <div className='filenew'>
-                            <Filetwo size={22} />
-                        </div> */}
+                        
                         <div className='chipheadertype'>{item?.templateInfo?.suggestions?.[0]?.title}</div>
                     </div>
                     <div className='mulanschip'>
@@ -24,52 +20,12 @@ const AgentWelcomeTemplate = (props) => {
                             return (
                                 <div className='chipone'
                                     onClick={()=> InvokeGptAgentTemplate({item, utterance})}
-                                    // onClick={() => {
-                                    //     if (!item?.templateInfo?.suggestions?.[0]?.comingSoon) {
-
-                                            
-
-
-                                    //         // let question = utterance?.label
-                                    //         // // if (utterance?.action === "createSession" && utterance?.source === "attachment") {
-                                    //         // //     {<BotConversation />}
-                                    //         // // }
-                                    //         // // }
-                                    //         // // if (utterance?.action === "createSession" && utterance?.source === "text") {
-                                    //         // //     return <SummarizeAgent />;
-                                    //         // if (utterance?.action === "createSession" && utterance?.source === "attachment") {
-                                    //         //     // setOpenUploadComp(true)
-                                    //         //     document.querySelector("#kiaasFileAttachment").click()
-                                    //         //     return
-                                    //         // }
-
-                                    //         // if (utterance?.action === "createSession" && utterance?.source === "text"){
-                                    //         //     eventBus.dispatch("displayOverlay")
-                                    //         //     return
-                                    //         // }
-                                    //         // window.universalSearch({ suggestionAfterConnection: true, question, selectedContext })
-                                    //         // if (utterance?.action === "createSession" && (utterance?.source === "translate" || utterance?.source === "summarizer")){
-                                    //         //     // <TranslateFromView />
-                                    //         //     // setTranslateComp(true)
-                                    //         //     window.universalSearch({ suggestionAfterConnection: true, question })
-                                    //         // }
-
-                                    //         // if (utterance?.action) {
-                                    //         //     window.universalSearch({ suggestionAfterConnection: true, question })
-                                    //         // }
-
-                                    //     }
-                                    // }}
                                     >
                                     <div className='leftBlock'>
-                                        {/* <span className='tickmarkicon'>
-                                            <TickMark color="#98A2B3" size={13} />
-                                        </span> */}
                                         <span className='newtext' key={i}>{utterance?.label}</span>
                                     </div>
                                     {item?.utterances?.isNew &&
                                         <div className='rightBlock'>
-                                            {/* <div className='rbIcon'><StarsIcon size={12} color='#12B76A' /></div> */}
                                             <div className='rbText'>New</div>
                                         </div>
                                     }
