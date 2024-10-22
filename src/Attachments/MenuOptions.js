0c397f2a52df8a2f50ceada866cdd01bb3f0ccaf
Shariqmo-kore <shariq.mohd@kore.com>
2024-10-22 15:51:38 +0530

    menu options removed

--- src/Attachments/MenuOptions.js
+++
@@ -1,8 +0,0 @@
-import FileUpload from "./fileUpload";
-
-const MenuOptions = (item) => {
-    const askFollowUpButton = document.getElementById('followupButton');
-    askFollowUpButton.addEventListener('click', (e) => FileUpload().setChipAsSource(item))
-}
-
-export default MenuOptions;
