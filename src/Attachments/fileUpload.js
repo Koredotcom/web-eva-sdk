8cbbb20c00edb4d9db1b625817d23738d768f3e5
santhosh <sai.vangaveti@kore.com>
2024-11-12 17:55:00 +0530

    exposed uploadFile as a seperate function

--- src/Attachments/fileUpload.js
+++ src/Attachments/fileUpload.js
@@ -275,7 +275,8 @@ const FileUpload = (props) => {
         // uploadSelectedFile,
         removeContext,
         setAttachmentContext,
-        askFollowup
+        askFollowup,
+        uploadFile
     }
 }
 
