6b66dc591c25076a7934794d49f7a0bcd43858f9
Shariqmo-kore <shariq.mohd@kore.com>
2024-12-17 13:27:51 +0530

    added my credentials

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from "./App";
 import { initializeSDK} from './index'; // Adjust the import according to your SDK setup
 
-const getAccessToken = 'VvbYelSwP3CBGYV0rKjWzodoD2l_h_xZ0lc0QEkJj10OuwmqsHTR4iZLVjzR5MHA'; 
+const getAccessToken = 'OM2E8kTERGXB61ChT8H8WOPlcPufKQM1TDwWe_twnZtXP9rXWNj-MEDMxAPpEQd4'; 
 initializeSDK({
     accessToken: getAccessToken,
     api_url: 'https://eva-qa.kore.ai/api/',
-    userId: 'u-c9d2b051-ca8c-53cf-a808-a1becbc4d981'
+    userId: 'u-ccdab3b7-4a58-523a-812a-b20c8fccb87d'
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
