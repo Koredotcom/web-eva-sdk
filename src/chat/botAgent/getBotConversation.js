8b1e987fac956509315bfd4060a431e077b8df9e
santhosh <sai.vangaveti@kore.com>
2025-04-22 02:31:30 +0530

    added synchronous mode communication support to sdk

--- src/chat/botAgent/getBotConversation.js
+++ src/chat/botAgent/getBotConversation.js
@@ -4,6 +4,7 @@ import store from "../../redux/store";
 import { getReqIdByMessageId } from "../../utils/helpers";
 import { updateChatData, setBotSDKInstance, setCurrentQuestion, setEnableKoreBotSDK } from "../../redux/globalSlice";
 import { advanceSearch } from "../../redux/actions/global.action";
+import { constructQuestionPostCall } from "../chat-utils";
 
 const BotConversation = (args) => {
     let state = store.getState().global
@@ -76,7 +77,7 @@ const BotConversation = (args) => {
         }
         if(detail?.action === "update"){         
             /*reqId only comes when updating the parentMessage clo */   
-            if (detail?.message?.hasOwnProperty('reqId')) {
+            if (detail?.message?.hasOwnProperty('reqId') && Object.keys(questions || {}).length > 0) {
                 const currentQuestion = Object.values(questions).find(ques => ques.reqId === detail.message.reqId)
                 if(currentQuestion?.historicalData){
                     question = questions?.[currentQuestion?.id]
@@ -114,7 +115,7 @@ const BotConversation = (args) => {
     }
 
 
-    const submitBotResponse = (data) => {
+    const submitBotResponse = async (data) => {
         /**
          * needed payload
          payload = {
@@ -145,7 +146,8 @@ const BotConversation = (args) => {
         /*need to add a loading state for the current question */
         addLoadingStateToCurrentQuestion(data?.cId, data?.messageId)
         console.log("params data: ", data)
-        store.dispatch(advanceSearch({ params, payload, userId: state?.profile?.data?.id || data?.userId}))
+        const res = await store.dispatch(advanceSearch({ params, payload, userId: state?.profile?.data?.id || data?.userId}))
+        constructQuestionPostCall(res, data?.cId)
     }
 
     const addLoadingStateToCurrentQuestion = (reqId, messageId) => {
