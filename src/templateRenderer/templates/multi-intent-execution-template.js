ab92a78f41def99e45d97d97c5255cbdf57135c8
Karthik <karthik.praturi@kore.com>
2025-05-08 17:10:01 +0530

    Icon Fixes

--- src/templateRenderer/templates/multi-intent-execution-template.js
+++ src/templateRenderer/templates/multi-intent-execution-template.js
@@ -6,6 +6,14 @@ import { encodeHtml } from "../utils/helper";
 
 import TemplateComponents from "./index";
 
+
+const assistantIconTemplate = () => {
+    return `<img src="https://www.google.com/imgres?q=green%20dot&imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2Fe2%2F29%2F50%2Fe22950b0a26db427c651dccba60ab62c.jpg&imgrefurl=https%3A%2F%2Fin.pinterest.com%2Fpin%2Fgreen-circle-logo--333547916161752097%2F&docid=C6G2J8KBo9zd2M&tbnid=Np_pFb0VPiJdvM&vet=12ahUKEwiE-dPf3JONAxV2cmwGHVL7JgcQM3oECG8QAA..i&w=735&h=752&hcb=2&ved=2ahUKEwiE-dPf3JONAxV2cmwGHVL7JgcQM3oECG8QAA" alt="Assistant Icon" height="20" width="20"/>`;
+}
+const userIconTemplate = () => {
+    return `<img src="https://www.google.com/imgres?q=green%20dot&imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2Fe2%2F29%2F50%2Fe22950b0a26db427c651dccba60ab62c.jpg&imgrefurl=https%3A%2F%2Fin.pinterest.com%2Fpin%2Fgreen-circle-logo--333547916161752097%2F&docid=C6G2J8KBo9zd2M&tbnid=Np_pFb0VPiJdvM&vet=12ahUKEwiE-dPf3JONAxV2cmwGHVL7JgcQM3oECG8QAA..i&w=735&h=752&hcb=2&ved=2ahUKEwiE-dPf3JONAxV2cmwGHVL7JgcQM3oECG8QAA" alt="User Icon" height="20" width="20"/>`;
+};
+
 function render(data) {
 
     let state = store?.getState()?.global;
@@ -25,10 +33,11 @@ function render(data) {
         ` : ''}
         `;
 
+        
     let body = `
         ${items?.executionPipeline?.map((task, index) => {
             task = {...task, ..._questions[task?._id]};
-            let html = TemplateRenderer.generateHTMLTemplate(task);
+            let html = TemplateRenderer.generateHTMLTemplate(task, {});
             return `
                 <div className='addNewLineWrapper'>
                   <div className='addNewLine'>
