c88545a7cc14595a9dc78cc2a344844733bb9b6c
santhosh <sai.vangaveti@kore.com>
2025-08-21 15:58:36 +0530

    function to render compose bar

---
+++ src/components/RenderComposeBar.js
@@ -0,0 +1,23 @@
+import ComposeBar from './ComposeBar.js';
+
+function RenderComposeBar(targetDiv, options = {}) {
+    // Default options
+    const defaultOptions = {
+        placeholder: 'Ask question...',
+        showQuickActions: true,
+        showNewButton: true,
+        showStopButton: true,
+        ...options
+    };
+
+    try {
+        // Create and return ComposeBar instance
+        const composeBar = new ComposeBar(targetDiv, defaultOptions);
+        return composeBar;
+    } catch (error) {
+        console.error('Failed to initialize ComposeBar:', error);
+        throw error;
+    }
+}
+
+export default RenderComposeBar;
---
+++ src/composebar/index.js
@@ -0,0 +1,6 @@
+// ComposeBar module exports
+export { default as ComposeBar } from '../components/ComposeBar.js';
+export { default as RenderComposeBar } from '../components/RenderComposeBar.js';
+
+// Make RenderComposeBar the default export for easy import
+export { default } from '../components/RenderComposeBar.js';
