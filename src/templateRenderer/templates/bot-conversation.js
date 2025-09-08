31dd50443e75c16c0d1e2ece49d32d2ecdb8001b
sreevathsav.ramisetty@kore.com <sreevathsav.ramisetty@kore.com>
2025-09-08 16:16:24 +0530

    bot and autonomous scroll fix

--- src/templateRenderer/templates/bot-conversation.js
+++ src/templateRenderer/templates/bot-conversation.js
@@ -17,9 +17,9 @@ function escapeHTML(str) {
 		?.replace(/'/g, "&#039;");
 }
 
-function renderUserQuestion(question, userIconTemplate) {
+function renderUserQuestion(question, userIconTemplate , conversation) {
 	if (question) {
-		return `<div class="message-bubble question">
+		return `<div class="message-bubble question" id = "${conversation?.messageId}">
 					<div class="message-content">
 						<div class="message-text">${encodeHtml(question)}</div>
 						${userIconTemplate ? userIconTemplate : ""}
@@ -34,7 +34,7 @@ function renderAssistantQuestion(conversation, assistantIconTemplate) {
 	if(conversation?.template_html){
 		question = conversation?.template_html;
 	}
-	return `<div class="bot-flex-wrapper">
+	return `<div class="bot-flex-wrapper answer-container">
 				${assistantIconTemplate}
 				<div class='answerCntr'>					
 					<div class="assistant-question-container ${conversation?.status === 'completed' ? 'completed-assistant-question-container' : ''}">
@@ -60,6 +60,7 @@ function createConversationHTML(
 						<div class="bot-icon-container">${assistantIconTemplate ? assistantIconTemplate : ""}</div>
 						<div class="message-container"><div class="loading-text">${encodeHtml(loadingText)}</div></div>
 					</div>
+					<div class="min-view-container"></div>
 			</div>`;
 		}
 		if (conversation?.templateType === "search_answer") {
@@ -68,9 +69,10 @@ function createConversationHTML(
 				content += `<br/>`;
 				content += renderUserQuestion(
 					conversation?.answer,
-					userIconTemplate
+					userIconTemplate,
+					conversation
 				);
-				content += `<br/>`;
+				// content += `<br/>`;
 			}
 		}	
 		if (conversation?.templateType === "bot_template") {
@@ -87,7 +89,7 @@ function createConversationHTML(
                 <div class="completed">
 					${renderAssistantQuestion(conversation, assistantIconTemplate)}
 					<br/>
-					${renderUserQuestion(conversation?.answer, userIconTemplate)}
+					${renderUserQuestion(conversation?.answer, userIconTemplate , conversation)}
 					<br/>
                 </div>
             `;
