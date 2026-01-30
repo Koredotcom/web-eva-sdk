c2b3fef005a95b17c4e55746c4acac8ce88b87a4
sreevathsav.ramisetty@kore.com <sreevathsav.ramisetty@kore.com>
2026-01-30 20:14:52 +0530

    bot template in agentic flow fix

--- src/multiIntentExecution/multiIntentExecution.js
+++ src/multiIntentExecution/multiIntentExecution.js
@@ -47,7 +47,7 @@ const MultiIntentExecution = (props) => {
           ...state.questions,
           [item?.reqId]: {
             ...item,
-            status: "in-progress"
+            status: q?.status || "in-progress"
           }
         }))
 
