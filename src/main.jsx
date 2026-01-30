0aea74e41928ac707ad77d0d981c78d8de46fdcf
karthikpr-kore <141908357+karthikpr-kore@users.noreply.github.com>
2026-01-30 15:53:26 +0530

    Main.jsx fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,12 @@ import { initializeSDK} from './index'; // Adjust the import according to your S
 
 
 
-const getAccessToken = 'u-f3abd2ad-4b0f-51ae-894e-5e8f45fb881a'; 
+const getAccessToken = 'bgKlPSpWIdHeyvFEeCTTDYFLgeBzFQaogc6sEVvLNt8HKvvbJG0dgbGt9H4lbT3s'; 
 initializeSDK({
     accessToken: getAccessToken,
     api_url: 'https://eva-qa.kore.ai/api/',
     presence_url: 'https://eva-qa.kore.ai/',
-    userId: "u-f3abd2ad-4b0f-51ae-894e-5e8f45fb881a",
+    userId: "u-c9d2b051-ca8c-53cf-a808-a1becbc4d981",
     initializeBotSDK:{
         "name": "ProcureBot",
         "streamId": "st-b6012ef2-810d-5240-b33e-5404d68b680e",
