f5ee32c116b8a14abaceeaec6aa90cd8d99350e7
Sai Santhosh <sai.vangaveti@kore.com>
2026-04-22 15:29:12 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 import { initializeSDK} from './index'; // Adjust the import according to your SDK setup
+import { authenticateApp } from './authorization';
 
 
 
 const getAccessToken = 'bgKlPSpWIdHeyvFEeCTTDYFLgeBzFQaogc6sEVvLNt8HKvvbJG0dgbGt9H4lbT3s'; 
 initializeSDK({
     accessToken: getAccessToken,
-    api_url: 'https://eva-qa.kore.ai/api/',
+    api_url: 'https://eva-qa.kore.ai/api/1.1/',
     presence_url: 'https://eva-qa.kore.ai/',
     userId: "u-c9d2b051-ca8c-53cf-a808-a1becbc4d981",
     initializeBotSDK:{
@@ -19,9 +20,16 @@ initializeSDK({
             "clientSecret": "VJNwkfbPcMZl4bOa1Qn3XtYRz6rqigwtTgOlaYX25Xs="
         },
     },
-    enableDebugging: false,
+    enableDebugging: true,
     autoRemoveWebSearchFromContext: false // this flag helps to set the context after advancedSearch
 });
+
+// authenticateApp({
+//   emailId: "sai.vangaveti@kore.com",
+//   id_token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzYWkudmFuZ2F2ZXRpQGtvcmUuY29tIiwiZmlyc3ROYW1lIjoic2FpIiwibGFzdE5hbWUiOiJzYW50aG9zaCIsImFwcElkIjoiY3MtNGE2MjZhYmEtMWU3ZS01NzQ2LTg0ZjMtYWVjZThkNzFkYTc5IiwiaWF0IjoxNzc2NzU1NTMwMjQ4LCJleHAiOjE3NzY3NTU2NTY5MTl9.PnFek9nZPwA2_SEyRqNE1YsLkKyhw8weD3QG2ihPsU0",
+// }).then((res) => {
+//   console.log(res);
+// });
 
 
 
