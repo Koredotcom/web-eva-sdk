5555e03ae82ec13415e0c3b6ca61b35b70439497
Sai Santhosh Kumar <sai.vangaveti@kore.com>
2026-08-13 13:08:16 +0530

    updated the subscription logic in chatInterface component so that it wount get updated multiple times once the status is turned to completed

--- src/redux/middleware/logger.js
+++ src/redux/middleware/logger.js
@@ -1,6 +1,8 @@
 const logger = storeAPI => next => action => {
     const enableDebugging = storeAPI.getState()?.global?.enableDebugging;
     if (enableDebugging) {
+        /* store subscribers are not handed the action, so stash the type for them to attribute triggers */
+        globalThis.__evaLastAction = action?.type;
         console.log('Dispatching action:', action);
     }
     const result = next(action);
